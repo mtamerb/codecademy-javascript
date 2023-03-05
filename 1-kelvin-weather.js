8e2ecbfab8287564cb7546d066b012928b10328f
Tamer <tamerbilici00@gmail.com>
2023-03-05 23:45:47 +0300

    refactor codes

--- 1-kelvin-weather.js
+++ 1-kelvin-weather.js
@@ -1,5 +1,7 @@
 const kelvin = 293;
 const celcius = kelvin - 273;
 let fahrenheit = celcius * (9 / 5) + 32;
+
 fahrenheit = Math.floor(fahrenheit);
+
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
--- 4-race-day.js
+++ 4-race-day.js
@@ -1,21 +1,23 @@
 let raceNumber = Math.floor(Math.random() * 1000);
 let isEarly;
 let racerAge = 19;
+
 racerAge < 18 ? (isEarly = true) : (isEarly = false);
+
 if (racerAge > 18 && isEarly === true) {
     raceNumber += 1000;
     console.log('Race at 9:30');
     console.log('race number : ' + raceNumber);
 }
-if( racerAge > 18 && isEarly === false){
+if (racerAge > 18 && isEarly === false) {
     console.log('Race at 11:00');
     console.log('race number : ' + raceNumber);
 }
-if(racerAge < 18){
+if (racerAge < 18) {
     console.log('Race at 12:30');
     console.log('race number : ' + raceNumber);
 }
-if(racerAge === 18){
+if (racerAge === 18) {
     console.log('See the registration desk !');
 }
 
