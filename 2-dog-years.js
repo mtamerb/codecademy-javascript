d1131d95eab943402d013137595cff23db0df1e2
Tamer <tamerbilici00@gmail.com>
2023-03-05 23:44:50 +0300

    race day solutions added

--- 2-dog-years.js
+++ 2-dog-years.js
@@ -1,8 +1,12 @@
 let myAge = 23;
 let earlyYears = 2;
+
 earlyYears *= 10.5;
+
 let laterYears = myAge - 2;
 laterYears *= 4;
+
 const dogAge = laterYears + earlyYears;
 const myName = 'Tamer'.toLowerCase();
+
 console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${dogAge} years old in dog years.`);
